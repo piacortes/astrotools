97b83fac3029893c1ed97b74965398c495a286db
Pia Cortes <pia.cortes@ug.uchile.cl>
2018-09-06 16:24:47 -0300

    minor bugs fixed

--- data.py
+++ data.py
@@ -48,7 +48,7 @@ def get_flux(input_file, planet_name, epoch):
     np.savetxt(planet_name + "_" + epoch + ".txt", np.array([time, flux, error]).T, delimiter = " ")
 
 def build_txt(time,flux,error, file_name):
-    fmt = "%.10f, %.10f, %.10f"
+    fmt = "%.10f %.10f %.10f"
     np.savetxt(file_name + ".txt", np.array([time,flux,error]).T, delimiter=" ", fmt=fmt)
 
 def get_bjd_tdb(time_utc, target, obs):
@@ -66,9 +66,63 @@ def get_bjd_tdb(time_utc, target, obs):
         ob = coo.EarthLocation.of_site("Las Campanas Observatory")
     if obs == "tololo":
         ob = coo.EarthLocation.of_site("Cerro Tololo Interamerican Observatory")
+    if obs == "extra":
+        ob = coo.EarthLocation.of_address("Girona, España")
 
     times = time.Time(time_utc, format="jd", scale="utc", location=ob)
     ltt_bary = times.light_travel_time(tg, ephemeris="jpl")
     bjd_tdb = times.tdb + ltt_bary
 
     return bjd_tdb.value
+
+def normalize_flux(date, flux):
+    """It normalizes the flux by the median of the part without transit"""
+
+    # Compute the mean value of flux
+    mean_flux = np.mean(flux)
+
+    # Determinate the points above the mean of the flux
+    flux_upper = flux[flux>=mean_flux]
+    date_upper = date[flux>=mean_flux]
+
+    # Compute the median of the points above the mean of the flux
+    median_flux = np.median(flux_upper)
+
+    # Normalize the flux by the median of the no_transit flux
+    norm_flux = flux / median_flux
+
+    return norm_flux
+
+def normalize_flux2(per, a, t0, time, flux):
+    """It normalizes the flux by the median of the part without transit"""
+
+    time = np.asarray(time)
+    flux = np.asarray(flux)
+
+    # Compute the transit duration
+    transit_duration = (per / np.pi) * np.arcsin(1 / a)
+
+    # Compute the half of the transit duration
+    half_transit = transit_duration / 2
+
+    # Make a boolean mask for the date array, is True for the transit duration
+    mask_transit = (-half_transit <= time) & (time <= half_transit)
+
+    # Make a boolean mask for the date array but now True is for the time without transit
+    mask_no_transit = np.logical_not(mask_transit)
+
+    # Get the flux point without transit
+    flux_no_transit = flux[mask_no_transit]
+
+    # Compute the median of the flux without transit
+    median_flux = np.median(flux_no_transit)
+
+    # Normalize the flux by the median of the no_transit flux
+    norm_flux = flux/median_flux
+
+    return norm_flux
+
+
+#time, flux, error = get_data(".txt")
+#time_bjd = get_bjd_tdb(time_utc=time, obs="lascampanas", target="WASP77")
+#build_txt(file_name="WASP77b_20161007_bjd", flux=flux,time=time_bjd,error=error)
